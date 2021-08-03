88b65e4dd382ca4181bd2c6a8b9ba1947c2cef14
abhi16180 <64468853+abhi16180@users.noreply.github.com>
2021-08-03 15:28:29 +0530

    second commit

--- lib/custom_button.dart
+++ lib/custom_button.dart
@@ -1,7 +1,83 @@
 library custom_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  //functions
+  void Function()? onTap;
+  void Function()? onLongPress;
+  void Function()? onTapCancel;
+  void Function()? onDoubleTap;
+  void Function(bool)? onHover;
+  void Function(bool)? onFocusChange;
+  void Function(bool)? onHighlightChanged;
+  void Function(TapDownDetails)? onTapDown;
+
+  //colors
+  Color? focusColor;
+  Color? hoverColor;
+  Color? splashColor;
+  Color? highlightColor;
+  Color backgroundColor;
+  MaterialStateProperty<Color>? overlayColor;
+  //child
+  Widget? child;
+  //Dimentions
+  double width;
+  double height;
+  // borderRadisu
+  BorderRadiusGeometry borderRadius;
+  Button({
+    this.onTap,
+    this.onHover,
+    this.onLongPress,
+    this.onTapCancel,
+    this.onTapDown,
+    this.onDoubleTap,
+    this.onFocusChange,
+    this.onHighlightChanged,
+    this.focusColor,
+    this.highlightColor,
+    this.overlayColor,
+    this.backgroundColor = Colors.yellow,
+    this.splashColor = Colors.amber,
+    this.hoverColor = Colors.yellowAccent,
+    required this.child,
+    required this.borderRadius,
+    this.height = 50,
+    this.width = 100,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      clipBehavior: Clip.antiAlias,
+      color: backgroundColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: borderRadius,
+      ),
+      child: Container(
+        width: width,
+        height: height,
+        child: InkWell(
+          focusColor: focusColor,
+          hoverColor: hoverColor,
+          splashColor: splashColor,
+          overlayColor: overlayColor,
+          highlightColor: highlightColor,
+          onTap: onTap,
+          onHover: onHover,
+          onDoubleTap: onDoubleTap,
+          onFocusChange: onFocusChange,
+          onLongPress: onLongPress,
+          onTapCancel: onTapCancel,
+          onTapDown: onTapDown,
+          onHighlightChanged: onHighlightChanged,
+          child: Center(
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
 }
